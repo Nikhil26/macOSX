71871a1ce6be8109b0d5ea3b30a08007c4476e79
Nikhil26 <nikhowale@gmail.com>
2018-12-13 17:59:32 +0530

    Added Test Mac OS X Project

---
+++ CircleCiTest/CircleCiTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CircleCiTest
+//
+//  Created by Nikhi Howale on 13/12/18.
+//  Copyright © 2018 Nikhil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
